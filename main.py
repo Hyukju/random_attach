d1d5bfa8f6ad02887055f86e634de6022ed50fb6
joo <hyukju1223@gmail.com>
2021-06-30 14:03:30 +0900

    add random attach using image

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ RECT_MIN_WIDTH = 50
 RECT_MAX_WIDTH = 100
 RECT_MIN_HEIGHT = 50
 RECT_MAX_HEIGHT = 100
-SUM_IOUS_MIN = -1.0
+SUM_IOUS_MIN = 0.0
 SUM_IOUS_MAX = 0.001
 
 def cal_iou(rect1, rect2):
@@ -44,83 +44,29 @@ def read_file_path_list(main_dir):
     return file_path_list
 
 
-def get_random_rect():
-    width =  np.random.randint(RECT_MIN_WIDTH, RECT_MAX_WIDTH)
-    height =  np.random.randint(RECT_MIN_HEIGHT, RECT_MAX_HEIGHT)
+def get_random_rect(rect=None):
+    if rect == None:
+        width =  np.random.randint(RECT_MIN_WIDTH, RECT_MAX_WIDTH)
+        height =  np.random.randint(RECT_MIN_HEIGHT, RECT_MAX_HEIGHT)
+    else:
+        width = rect[2]
+        height = rect[3]
 
-    x = np.random.randint(CANVAS_MARGIN, CANVAS_WIDTH - width - CANVAS_MARGIN)
-    y = np.random.randint(CANVAS_MARGIN, CANVAS_HEIGHT - height - CANVAS_MARGIN)
+    x = np.random.randint(CANVAS_MARGIN, CANVAS_WIDTH - CANVAS_MARGIN)
+    y = np.random.randint(CANVAS_MARGIN, CANVAS_HEIGHT - CANVAS_MARGIN)
 
     return x, y, width, height
 
-def get_random_rect2(rect):
-    width = rect[2]
-    height = rect[3]
-    x = np.random.randint(CANVAS_MARGIN, CANVAS_WIDTH - width)
-    y = np.random.randint(CANVAS_MARGIN, CANVAS_HEIGHT - height)
-    return x, y, width ,height
+def get_random_rects(num_rects=None, in_rects=None, use_debug=False):
+    assert not (num_rects==None and in_rects==None)
 
-def get_random_rects(n):
-    rects = []
-    # get first rect
-    rects.append(get_random_rect())
-
-    back = np.zeros((CANVAS_HEIGHT,CANVAS_WIDTH))
-
-    # insert first rect in background
-    for rect in rects:
-        x, y, width, height = rect[0],rect[1],rect[2],rect[3]
-        back[y:y+height, x:x+width] = 1    
-
-    for _ in range(n):
-
-        # get random width, height of other rects       
-        width =  np.random.randint(RECT_MIN_WIDTH, RECT_MAX_HEIGHT)
-        height =  np.random.randint(RECT_MIN_HEIGHT, RECT_MAX_HEIGHT)
-
-        # get zero point in background
-        zero_y, zero_x = np.where(back == 0)
-       
-        # get indices of zeros points
-        zero_indices = list(range(len(zero_x)))
-        print('# zero_indices = ', len(zero_indices))
-
-        # check rects postions
-        cv2.namedWindow('rects',0)
-        cv2.imshow('rects', back)
-        cv2.waitKey(1000) # ms
-        
-        while True:   
-            sum_ious = 0   
-            # get random index in zero positions
-            idx = random.choice(zero_indices)
-            x = zero_x[idx]
-            y = zero_y[idx]
-
-            print('x, y, width, height = {0},{1},{2},{3}'.format(x, y, width, height))
-
-            # check rect size and position
-            if x + width < CANVAS_WIDTH and y + height < CANVAS_HEIGHT:
-                # calculate iou sum to adjust overlap for each rect
-                for rect in rects:
-                    iou = cal_iou(rect, [x, y, width, height])
-                    sum_ious += iou  
-                print('sum ious = {0:.4f}'.format(sum_ious))    
-                                         
-                if sum_ious > SUM_IOUS_MIN and sum_ious < SUM_IOUS_MAX:                
-                    back[y:y+height, x:x+width,] = 1                          
-                    rects.append([x, y, width, height])
-                    break
-            # remove selected index before update
-            zero_indices.remove(idx)
-           
-    return rects
-
-
-def get_random_rects2(in_rects):
     out_rects = []
     # get first rect
-    out_rects.append(get_random_rect2(in_rects[0]))
+    if in_rects == None:
+        out_rects.append(get_random_rect())
+    else:
+        out_rects.append(get_random_rect(in_rects[0]))
+        num_rects = len(in_rects)
 
     back = np.zeros((CANVAS_HEIGHT,CANVAS_WIDTH))
 
@@ -129,47 +75,63 @@ def get_random_rects2(in_rects):
         x, y, width, height = out_rect[0],out_rect[1],out_rect[2],out_rect[3]
         back[y:y+height, x:x+width] = 1    
 
-    for in_rect in in_rects[1:]:
+    try:
+        for i in range(1,num_rects):
 
-        # get random width, height of other rects       
-        width =  in_rect[2]
-        height =  in_rect[3]
+            # get random width, height of other rects     
+            if in_rects == None:  
+                width =  np.random.randint(RECT_MIN_WIDTH, RECT_MAX_HEIGHT)
+                height =  np.random.randint(RECT_MIN_HEIGHT, RECT_MAX_HEIGHT)
+            else:
+                in_rect = in_rects[i]
+                width = in_rect[2]
+                height = in_rect[3]
 
-        # get zero point in background
-        zero_y, zero_x = np.where(back == 0)
-       
-        # get indices of zeros points
-        zero_indices = list(range(len(zero_x)))
-        # print('# zero_indices = ', len(zero_indices))
-
-        # # check rects postions
-        # cv2.namedWindow('rects',0)
-        # cv2.imshow('rects', back)
-        # cv2.waitKey(1000) # ms
+            # get zero point in background
+            zero_y, zero_x = np.where(back == 0)
         
-        while True:   
-            sum_ious = 0   
-            # get random index in zero positions
-            idx = random.choice(zero_indices)
-            x = zero_x[idx]
-            y = zero_y[idx]
+            # get indices of zeros points
+            zero_indices = list(range(len(zero_x)))
 
-            # print('x, y, width, height = {0},{1},{2},{3}'.format(x, y, width, height))
+            if use_debug:
+                print('# zero_indices = ', len(zero_indices))
+                # check rects postions
+                cv2.namedWindow('preview',0)
+                cv2.imshow('preview', back)
+                cv2.waitKey(1000) # ms            
+            
+            while True:   
+                sum_ious = 0   
+                # get random index in zero positions
+                idx = random.choice(zero_indices)
+                x = zero_x[idx]
+                y = zero_y[idx]
 
-            # check rect size and position
-            if x + width < CANVAS_WIDTH and y + height < CANVAS_HEIGHT:
-                # calculate iou sum to adjust overlap for each rect
-                for out_rect in out_rects:
-                    iou = cal_iou(out_rect, [x, y, width, height])
-                    sum_ious += iou  
-                # print('sum ious = {0:.4f}'.format(sum_ious))    
-                                         
-                if sum_ious > SUM_IOUS_MIN and sum_ious < SUM_IOUS_MAX:                
-                    back[y:y+height, x:x+width,] = 1                          
-                    out_rects.append([x, y, width, height])
-                    break
-            # remove selected index before update
-            zero_indices.remove(idx)
+                if use_debug:
+                    print('x, y, width, height = {0},{1},{2},{3}'.format(x, y, width, height))
+
+                # check rect size and position
+                if x + width < CANVAS_WIDTH and y + height < CANVAS_HEIGHT:
+                    # calculate iou sum to adjust overlap for each rect
+                    for out_rect in out_rects:
+                        iou = cal_iou(out_rect, [x, y, width, height])
+                        sum_ious += iou  
+
+                        if use_debug:
+                            print('sum ious = {0:.4f}'.format(sum_ious))    
+                                            
+                    if sum_ious >= SUM_IOUS_MIN and sum_ious <= SUM_IOUS_MAX:                
+                        back[y:y+height, x:x+width,] = 1                          
+                        out_rects.append([x, y, width, height])
+                        break
+                # remove selected index before update
+                zero_indices.remove(idx)
+    except Exception as e:
+        print(e)
+        out_rects = None        
+    finally:
+        if use_debug:
+            cv2.destroyWindow('preview')
            
     return out_rects
 
@@ -207,8 +169,7 @@ if __name__=='__main__':
         rects = [] 
         polys = []
     
-
-        rects = get_random_rects(4)
+        rects = get_random_rects(4, use_debug=True)
 
         for rect in rects:
             poly = convert_rectangle_to_polygon(*rect)
